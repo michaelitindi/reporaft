4143107bab2584a90d659b6c6a661bd98c115ebe
michaelitindi <michaelitindi@gmail.com>
2025-09-02 10:20:36 +0300

    updated tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,21 +1,22 @@
-const colors = require('tailwindcss/colors')
+import colors from 'tailwindcss/colors'
+import typography from '@tailwindcss/typography'
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	darkMode: "class",
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
-        sans: ['Gabarito'],
-      },
-			colors: {
-				primary: colors.sky,
-				gray: colors.zinc,
-			}
-		},
-	},
-	plugins: [
-		require('@tailwindcss/typography'),
-	],
+    darkMode: "class",
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Gabarito'],
+            },
+            colors: {
+                primary: colors.sky,
+                gray: colors.zinc,
+            }
+        },
+    },
+    plugins: [
+        typography,
+    ],
 }
